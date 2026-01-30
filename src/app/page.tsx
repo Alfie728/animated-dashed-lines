87f785596b9c34df856cde6bedf341cb379a41aa
Alfie728 <zegro728@gmail.com>
2026-01-30 12:16:44 +1100

    adjusted glowSize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { DashedLinesScene } from "./components/dashed-lines-scene";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-white dark:bg-[#0a0a0a]">
+    <div className="flex min-h-screen items-center justify-center bg-[#f8f8f8] dark:bg-[#0a0a0a]">
       <div className="w-full max-w-3xl aspect-[400/94]">
         <DashedLinesScene />
       </div>
